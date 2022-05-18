fd9a0efae27cbd6022cb5d18d8bfaa24c9d8319b
Brad Bieselin <bradley.bieselin@gmail.com>
2022-05-17 22:16:18 -0400

    commit tree cc5c042cb8dd2f2104e938d66fbaf9fa49edfbf9

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,32 @@
-# your code goes here
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(happiness)
+        @happiness = happiness.clamp(0,10)
+    end
+
+    def hygiene=(hygiene)
+        @hygiene = hygiene.clamp(0,10)
+    end
+
+    def clean?
+        if hygiene <= 7
+            false
+        else hygiene > 7
+            true
+        end
+    end
+
+end
+
+brad = Person.new("Brad")
+brad.happiness=6
+p(brad)
